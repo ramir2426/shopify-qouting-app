00fa73e8b69dda994ddc0a863f4a227d34ded81c
Rana Amir <rana-amir@bayt.net>
2020-04-06 22:34:32 +0500

    change email address

--- app/mailers/quote_mailer.rb
+++ app/mailers/quote_mailer.rb
@@ -1,6 +1,6 @@
 class QuoteMailer < ApplicationMailer
 	def welcome_email(products, customer_info)
-		email = 'ramir2426@gmail.com'
+		email = 'info@htavpros.com'
 		@products = products
 		@customer_info = customer_info
 		mail(to: email, subject: 'Request to Product Quote')
